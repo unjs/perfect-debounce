46bc53a75971f07600dde0d4ed38c86129b45de5
Pooya Parsa <pooya@pi0.io>
2023-05-04 01:10:38 +0200

    chore: update repo

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ export interface DebounceOptions {
 }
 
 const DEBOUNCE_DEFAULTS: DebounceOptions = {
-  trailing: true
-}
+  trailing: true,
+};
 
 /**
 Debounce functions
@@ -35,76 +35,75 @@ for (const number of [1, 2, 3]) {
 //=> 3
 ```
 */
-export function debounce <ArgumentsT extends unknown[], ReturnT> (
+export function debounce<ArgumentsT extends unknown[], ReturnT>(
   fn: (...args: ArgumentsT) => PromiseLike<ReturnT> | ReturnT,
-  wait: number = 25,
+  wait = 25,
   options: DebounceOptions = {}
 ) {
   // Validate options
-  options = { ...DEBOUNCE_DEFAULTS, ...options }
+  options = { ...DEBOUNCE_DEFAULTS, ...options };
   if (!Number.isFinite(wait)) {
-    throw new TypeError('Expected `wait` to be a finite number')
+    throw new TypeError("Expected `wait` to be a finite number");
   }
 
   // Last result for leading value
-  let leadingValue: PromiseLike<ReturnT> | ReturnT
+  let leadingValue: PromiseLike<ReturnT> | ReturnT;
 
   // Debounce timeout handle
-  let timeout: NodeJS.Timeout
+  let timeout: NodeJS.Timeout;
 
   // Promises to be resolved when debounce if finished
-  let resolveList: Array<(val: unknown) => void> = []
+  let resolveList: Array<(val: unknown) => void> = [];
 
   // Keep state of currently resolving promise
-  let currentPromise: Promise<ReturnT>
+  let currentPromise: Promise<ReturnT>;
 
   // Trailing call info
-  let trailingArgs: any[]
+  let trailingArgs: any[];
 
-  const applyFn = async (_this, args) => {
-    currentPromise = _applyPromised(fn, _this, args)
-    currentPromise
-      .finally(() => {
-        currentPromise = null
-        if (options.trailing && trailingArgs && !timeout) {
-          const promise = applyFn(_this, trailingArgs)
-          trailingArgs = null
-          return promise
-        }
-      })
-    return currentPromise
-  }
+  const applyFn = (_this, args) => {
+    currentPromise = _applyPromised(fn, _this, args);
+    currentPromise.finally(() => {
+      currentPromise = null;
+      if (options.trailing && trailingArgs && !timeout) {
+        const promise = applyFn(_this, trailingArgs);
+        trailingArgs = null;
+        return promise;
+      }
+    });
+    return currentPromise;
+  };
 
   return function (...args: ArgumentsT) {
     if (currentPromise) {
       if (options.trailing) {
-        trailingArgs = args
+        trailingArgs = args;
       }
-      return currentPromise
+      return currentPromise;
     }
     return new Promise<ReturnT>((resolve) => {
-      const shouldCallNow = !timeout && options.leading
+      const shouldCallNow = !timeout && options.leading;
 
-      clearTimeout(timeout)
+      clearTimeout(timeout);
       timeout = setTimeout(() => {
-        timeout = null
-        const promise = options.leading ? leadingValue : applyFn(this, args)
+        timeout = null;
+        const promise = options.leading ? leadingValue : applyFn(this, args);
         for (const _resolve of resolveList) {
-          _resolve(promise)
+          _resolve(promise);
         }
-        resolveList = []
-      }, wait)
+        resolveList = [];
+      }, wait);
 
       if (shouldCallNow) {
-        leadingValue = applyFn(this, args)
-        resolve(leadingValue)
+        leadingValue = applyFn(this, args);
+        resolve(leadingValue);
       } else {
-        resolveList.push(resolve)
+        resolveList.push(resolve);
       }
-    })
-  }
+    });
+  };
 }
 
-async function _applyPromised (fn: Function, _this: unknown, args: any[]) {
-  return await fn.apply(_this, args)
+async function _applyPromised(fn: () => any, _this: unknown, args: any[]) {
+  return await fn.apply(_this, args);
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,127 +1,151 @@
-import { setTimeout as delay } from 'timers/promises'
-import { test, expect } from 'vitest'
-import inRange from 'in-range'
-import timeSpan from 'time-span'
-import { debounce } from '../src'
+import { setTimeout as delay } from "node:timers/promises";
+import { test, expect } from "vitest";
+import inRange from "in-range";
+import timeSpan from "time-span";
+import { debounce } from "../src";
 
-const fixture = 'fixture'
+const fixture = "fixture";
 
-test.concurrent('single call', async () => {
-  const debounced = debounce(async value => value, 100)
-  expect(await debounced(fixture)).toBe(fixture)
-})
+test.concurrent("single call", async () => {
+  const debounced = debounce(async (value) => value, 100);
+  expect(await debounced(fixture)).toBe(fixture);
+});
 
-test.concurrent('multiple calls', async () => {
-  let count = 0
-  const end = timeSpan()
-
-  const debounced = debounce(async (value) => {
-    count++
-    await delay(50)
-    return value
-  }, 100)
-
-  const results = await Promise.all([1, 2, 3, 4, 5].map(value => debounced(value)))
-
-  expect(results).toMatchObject([5, 5, 5, 5, 5])
-  expect(count).toBe(1)
-  expect(inRange(end(), {
-    start: 130,
-    end: 170
-  })).toBe(true)
-
-  await delay(110)
-  expect(await debounced(6)).toBe(6)
-})
-
-test.concurrent('leading option', async () => {
-  let count = 0
+test.concurrent("multiple calls", async () => {
+  let count = 0;
+  const end = timeSpan();
 
   const debounced = debounce(async (value) => {
-    count++
-    await delay(50)
-    return value
-  }, 100, { leading: true })
+    count++;
+    await delay(50);
+    return value;
+  }, 100);
 
-  const results = await Promise.all([1, 2, 3, 4].map(value => debounced(value)))
+  const results = await Promise.all(
+    [1, 2, 3, 4, 5].map((value) => debounced(value))
+  );
+
+  expect(results).toMatchObject([5, 5, 5, 5, 5]);
+  expect(count).toBe(1);
+  expect(
+    inRange(end(), {
+      start: 130,
+      end: 170,
+    })
+  ).toBe(true);
+
+  await delay(110);
+  expect(await debounced(6)).toBe(6);
+});
+
+test.concurrent("leading option", async () => {
+  let count = 0;
+
+  const debounced = debounce(
+    async (value) => {
+      count++;
+      await delay(50);
+      return value;
+    },
+    100,
+    { leading: true }
+  );
+
+  const results = await Promise.all(
+    [1, 2, 3, 4].map((value) => debounced(value))
+  );
 
   // value from the first promise is used without the timeout
-  expect(results).toEqual([1, 1, 1, 1])
-  expect(count).toBe(1)
+  expect(results).toEqual([1, 1, 1, 1]);
+  expect(count).toBe(1);
 
-  await delay(200)
-  expect(await debounced(5)).toBe(5)
-  expect(await debounced(6)).toBe(5)
-})
+  await delay(200);
+  expect(await debounced(5)).toBe(5);
+  expect(await debounced(6)).toBe(5);
+});
 
-test.concurrent('before option - does not call input function after timeout', async () => {
-  let count = 0
+test.concurrent(
+  "before option - does not call input function after timeout",
+  async () => {
+    let count = 0;
 
-  const debounced = debounce(async () => {
-    count++
-  }, 100, { leading: true })
+    const debounced = debounce(
+      async () => {
+        count++;
+      },
+      100,
+      { leading: true }
+    );
 
-  await delay(300)
-  await debounced()
+    await delay(300);
+    await debounced();
 
-  expect(count).toBe(1)
-})
+    expect(count).toBe(1);
+  }
+);
 
-test.concurrent('fn takes longer than wait', async () => {
-  let count = 0
+test.concurrent("fn takes longer than wait", async () => {
+  let count = 0;
 
   const debounced = debounce(async (value) => {
-    count++
-    await delay(200)
-    return value
-  }, 100)
+    count++;
+    await delay(200);
+    return value;
+  }, 100);
 
-  const setOne = [1, 2, 3]
-  const setTwo = [4, 5, 6]
+  const setOne = [1, 2, 3];
+  const setTwo = [4, 5, 6];
 
-  const promiseSetOne = setOne.map(value => debounced(value))
-  await delay(101)
-  const promiseSetTwo = setTwo.map(value => debounced(value))
+  const promiseSetOne = setOne.map((value) => debounced(value));
+  await delay(101);
+  const promiseSetTwo = setTwo.map((value) => debounced(value));
 
-  const results = await Promise.all([...promiseSetOne, ...promiseSetTwo])
+  const results = await Promise.all([...promiseSetOne, ...promiseSetTwo]);
 
-  expect(results).toMatchObject([3, 3, 3, 3, 3, 3])
-  expect(count).toBe(2)
-})
+  expect(results).toMatchObject([3, 3, 3, 3, 3, 3]);
+  expect(count).toBe(2);
+});
 
 // Factory to create a separate class for each test below
 // * Each test replaces methods in the class with a debounced variant,
 //   hence the need to start with fresh class for each test.
-const createFixtureClass = () => class {
-  _foo = 'fixture'
+const createFixtureClass = () =>
+  class {
+    _foo = "fixture";
 
-  foo () {
-    // If `this` is not preserved by `debounce()` or `debounce.promise()`,
-    // then `this` will be undefined and accessing `this._foo` will throw.
-    return this._foo
-  }
+    foo() {
+      // If `this` is not preserved by `debounce()` or `debounce.promise()`,
+      // then `this` will be undefined and accessing `this._foo` will throw.
+      return this._foo;
+    }
 
-  getThis () {
-    // If `this` is not preserved by `debounce()` or `debounce.promise()`,
-    // then `this` will be undefined.
-    return this
-  }
-}
+    getThis() {
+      // If `this` is not preserved by `debounce()` or `debounce.promise()`,
+      // then `this` will be undefined.
+      return this;
+    }
+  };
 
-test.concurrent('`this` is preserved ', async () => {
-  const FixtureClass = createFixtureClass()
-  FixtureClass.prototype.foo = (debounce as Function)(FixtureClass.prototype.foo, 10)
-  FixtureClass.prototype.getThis = (debounce as Function)(FixtureClass.prototype.getThis, 10)
+test.concurrent("`this` is preserved ", async () => {
+  const FixtureClass = createFixtureClass();
+  FixtureClass.prototype.foo = (debounce as Function)(
+    FixtureClass.prototype.foo,
+    10
+  );
+  FixtureClass.prototype.getThis = (debounce as Function)(
+    FixtureClass.prototype.getThis,
+    10
+  );
 
-  const thisFixture = new FixtureClass()
+  const thisFixture = new FixtureClass();
 
-  expect(await thisFixture.getThis()).toBe(thisFixture)
-  expect(() => thisFixture.foo()).not.throws()
-  expect(await thisFixture.foo()).toBe(fixture)
-})
+  expect(await thisFixture.getThis()).toBe(thisFixture);
+  expect(() => thisFixture.foo()).not.throws();
+  expect(await thisFixture.foo()).toBe(fixture);
+});
 
-test.concurrent('wait for promise', async () => {
-  const results = []
+test.concurrent("wait for promise", async () => {
+  const results = [];
 
   /*
 Time:      000---025---050---075---100---125--150---175---200---225---250---275---300---325--350---400-----> (ms)
@@ -132,34 +156,34 @@ Trailing:              T=2          T=3        T=4         T=5        T=6
 Resolves:  R=1         R=1         R=1        R=3         R=3         R=5
 */
 
-  const EXEC_MS = 100
-  const DEBOUNCE_MS = 25
-  const REPEAT_MS = 50
+  const EXEC_MS = 100;
+  const DEBOUNCE_MS = 25;
+  const REPEAT_MS = 50;
 
   const debounced = debounce(async (value) => {
-    await delay(EXEC_MS)
-    results.push(value)
-    return value
-  }, DEBOUNCE_MS)
+    await delay(EXEC_MS);
+    results.push(value);
+    return value;
+  }, DEBOUNCE_MS);
 
-  const promises = []
+  const promises = [];
   for (const i of [1, 2, 3, 4, 5, 6]) {
-    promises.push(debounced(i))
-    await delay(REPEAT_MS)
+    promises.push(debounced(i));
+    await delay(REPEAT_MS);
   }
-  const resolvedResults = await Promise.all(promises)
+  const resolvedResults = await Promise.all(promises);
 
-  await delay(EXEC_MS)
+  await delay(EXEC_MS);
 
   // console.log('Results:', results)
   // console.log('Resolved results:', resolvedResults)
 
-  expect(results).toMatchObject([1, 3, 5, 6])
-  expect(resolvedResults).toMatchObject([1, 1, 1, 3, 3, 5])
-})
+  expect(results).toMatchObject([1, 3, 5, 6]);
+  expect(resolvedResults).toMatchObject([1, 1, 1, 3, 3, 5]);
+});
 
-test.concurrent('wait for promise (leading: true)', async () => {
-  const results = []
+test.concurrent("wait for promise (leading: true)", async () => {
+  const results = [];
 
   /*
 Time:      000---025---050---075---100---125--150---175---200---225---250---275---300---325--350---400-----> (ms)
@@ -170,24 +194,28 @@ Trailing:              T=2         T=3         T=4        T=5.........T=6
 Resolves:  R=1         R=1         R=2        R=2         R=4         R=4
 */
 
-  const EXEC_MS = 100
-  const DEBOUNCE_MS = 25
-  const REPEAT_MS = 50
+  const EXEC_MS = 100;
+  const DEBOUNCE_MS = 25;
+  const REPEAT_MS = 50;
 
-  const debounced = debounce(async (value) => {
-    await delay(EXEC_MS)
-    results.push(value)
-    return value
-  }, DEBOUNCE_MS, { leading: true })
+  const debounced = debounce(
+    async (value) => {
+      await delay(EXEC_MS);
+      results.push(value);
+      return value;
+    },
+    DEBOUNCE_MS,
+    { leading: true }
+  );
 
-  const promises = []
+  const promises = [];
   for (const i of [1, 2, 3, 4, 5, 6]) {
-    promises.push(debounced(i))
-    await delay(REPEAT_MS)
+    promises.push(debounced(i));
+    await delay(REPEAT_MS);
   }
-  const resolvedResults = await Promise.all(promises)
+  const resolvedResults = await Promise.all(promises);
 
-  await delay(EXEC_MS)
+  await delay(EXEC_MS);
 
   // console.log('Results:', results)
   // console.log('Resolved results:', resolvedResults)
@@ -195,8 +223,8 @@ Resolves:  R=1         R=1         R=2        R=2         R=4         R=4
   // TODO: Results order are unexpected
   // expect(results).toMatchObject([1, 2, 4, 6])
   // expect(resolvedResults).toMatchObject([1, 1, 2, 2, 4, 4])
-  expect(results.length).toBe(4)
-  expect(resolvedResults.length).toBe(6)
-  expect(results[0]).toBe(1)
-  expect(results[3]).toBe(6)
-})
+  expect(results.length).toBe(4);
+  expect(resolvedResults.length).toBe(6);
+  expect(results[0]).toBe(1);
+  expect(results[3]).toBe(6);
+});
